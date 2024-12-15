be0b7a00af95cba24d6692b9a5f78b24df3e20d2
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2024-12-14 19:58:05 -0600

    Adding comments

--- cpp/src/resize.cpp
+++ cpp/src/resize.cpp
@@ -1,3 +1,8 @@
+/*
+Author: Emmanuel A. Larralde Ortiz
+Description:
+    Resizes an image using lanczos resampling interpolation.
+*/
 #include <iostream>
 #include "../include/pgm/pgm.h"
 #include "../include/lanczos/lanczos.h"
@@ -5,18 +10,25 @@
 using namespace std;
 
 int main(int argc, char **argv){
+    //Agrs safeguard
     if(argc < 3)
         return 0;
 
-    string src_path(argv[1]);
-    string dst_path(argv[2]);
+    //Args parsing
+    string src_path(argv[1]); //Source image path
+    string dst_path(argv[2]); //Destination image path
 
+    //Load soruce image
     Img img = read_pgm(src_path);
-    
+
+    //get source image size
     int rows = img.size();
     int cols = img[0].size();
 
+    //lanczos resize.
     Img new_image = lanczos_resize(img, rows*M_PI, cols*M_PI);
+
+    //Write back new image.
     write_pgm(dst_path, new_image);
 
     return 0;
